ca3ba7e5e2f6d8b57da25bc9878123fb3bd23688
unknown <patticus.tv@gmail.com>
2021-12-07 02:38:42 +1100

    fixed typo in special characters array, removed un needed variable and added confirm buttons for all character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","'","<",">",",","?",","/"];
-
-var userInput = prompt("Please select your password criteria");
-
+var numberChar = ["0","1","2","3","4","5","6","7","8","9"];
+var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","'","<",">",",","?",",","/"];
 
 // Write password to the #password input
 function writePassword() {
@@ -27,7 +24,10 @@ function generatePassword() {
 
   }
 
-  var specialChar = confirm( "do you want special chars")
+  var numberChar = confirm("Do you want numbers?")
+  var lowerChar = confirm("Do you want lower case characters?")
+  var upperChar = confirm("Do you want upper case characters?")
+  var specialChar = confirm("Do you want special characters?")
 
 
 
