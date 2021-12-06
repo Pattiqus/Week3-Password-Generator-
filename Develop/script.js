cb30ee93bc351a0991294cf30372478f513c10af
unknown <patticus.tv@gmail.com>
2021-12-07 02:13:21 +1100

    Changed password character to an array, cannot get prompt to activate

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = ["0123456789"];
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special = "~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/";
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","'","<",">",",","?",","/"];
+
 var userInput = prompt("Please select your password criteria");
-var length = 
+
 
 // Write password to the #password input
 function writePassword() {
@@ -26,11 +27,12 @@ function generatePassword() {
 
   }
 
-  var specialChar = confirm( "do you want special chars"
+  var specialChar = confirm( "do you want special chars")
+
 
 
   
-  createPassword())
+  createPassword()
 
 }
 
